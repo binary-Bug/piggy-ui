3ac7d00e376c7e4e85791d5b9b85ea93e32caac4
binary-Bug <punitbantia@gmail.com>
2024-05-14 22:58:49 +0530

    inital commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -8,6 +10,23 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
+
+
 export class AppComponent {
   title = 'AngularTest';
+
+  constructor(private http: HttpClient ){}
+
+  getData(): Observable<any> {
+    return this.http.get<any>("https://localhost:7193/WeatherForecast");
+  }
+
+  apicall() : void{
+    this.getData().subscribe(data => {
+      let summary = data[0].summary;
+      console.log(data[0].summary);
+    })
+
+  }
 }
