aed2c3acbf4800560462ea49beb4a2f43bbacdb7
binary-Bug <punitbantia@gmail.com>
2024-08-19 02:03:00 +0530

    code format

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,23 +9,20 @@ import { apiurl, localapiurl } from '../configs/config';
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-
-
-
 export class AppComponent {
   title = 'AngularTest';
 
-  constructor(private http: HttpClient ){}
+  constructor(private http: HttpClient) {}
 
   getData(): Observable<any> {
-    return this.http.get<any>(apiurl+"weatherforecast/get/v2");
+    return this.http.get<any>(apiurl + 'weatherforecast/get/v2');
   }
 
-  apicall(): void{
-    this.getData().subscribe(data => {
-      (data as Array<any>).forEach(d => console.log(d.summary));
+  apicall(): void {
+    this.getData().subscribe((data) => {
+      (data as Array<any>).forEach((d) => console.log(d.summary));
     });
   }
 }
