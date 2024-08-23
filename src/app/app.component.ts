fe573b4965aa049bb6ae727c103bb00fe475fb84
ashar708 <aarya_sharma@optum.com>
2024-08-23 20:20:11 +0530

    welcome page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,26 +3,15 @@ import { RouterOutlet } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { apiurl, localapiurl } from '../configs/config';
+import { WelcomeComponent } from './pages/WelcomePage/welcome/welcome.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,WelcomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'AngularTest';
-
-  constructor(private http: HttpClient) {}
-
-  getData(): Observable<any> {
-    return this.http.get<any>(apiurl + 'weatherforecast/get/v2');
-  }
-
-  apicall(): void {
-    this.getData().subscribe((data) => {
-      (data as Array<any>).forEach((d) => console.log(d.summary));
-    });
-  }
+  
 }
