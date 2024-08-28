b2b9cd1777b3973606cb789d17b3a886c46388e8
Punit A Bantia <a_bantia@optum.com>
2024-08-28 20:03:14 +0530

    updated form component

--- src/app/pages/WelcomePage/welcome/welcome.component.ts
+++ src/app/pages/WelcomePage/welcome/welcome.component.ts
@@ -2,11 +2,12 @@ import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { Observable } from 'rxjs/internal/Observable';
 import { apiurl } from '../../../../configs/config';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-welcome',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './welcome.component.html',
   styleUrl: './welcome.component.css'
 })
@@ -14,8 +15,21 @@ export class WelcomeComponent {
 
   constructor(private http: HttpClient) {}
 
+  isRegister : boolean = false;
+
   getData(): Observable<any> {
     return this.http.get<any>(apiurl + 'weatherforecast/get/v2');
+  }
+
+  onToggleForm(): void {
+    this.isRegister = !this.isRegister;
+  }
+
+  updateClass(ele: HTMLDivElement): void {
+    if(this.isRegister)
+    ele.style.height = "480px";
+  else 
+  ele.style.height = "420px";
   }
 
   apicall(): void {
