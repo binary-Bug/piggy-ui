1c544d5998f93d340b7ce55cd0a8aa29adc7eacd
binary-Bug <punitbantia@gmail.com>
2024-09-17 03:00:26 +0530

    Added ngx spinner package and added loading spinner while connecting to db

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { WelcomeComponent } from './pages/welcome/welcome.component';
+import { HttpClient } from '@angular/common/http';
+import { apiurl } from '../configs/config';
+import { CommonModule } from '@angular/common';
+import { NgxSpinnerModule } from 'ngx-spinner';
+import { NgxSpinnerService } from 'ngx-spinner';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,WelcomeComponent],
+  imports: [RouterOutlet, WelcomeComponent, CommonModule, NgxSpinnerModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {
-  
+export class AppComponent implements OnInit {
+  http = inject(HttpClient);
+  spinner = inject(NgxSpinnerService);
+  ngOnInit() {
+    this.spinner.show();
+    this.http
+      .get<any>(apiurl + 'establish_api_and_dbconnection')
+      .subscribe((res) => {
+        console.log(res);
+        this.spinner.hide();
+      });
+  }
 }
