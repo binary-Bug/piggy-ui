187927f2dc2cc78042904915f74786b79933de4e
binary-Bug <punitbantia@gmail.com>
2024-10-07 06:19:18 +0530

    updated config

--- src/configs/config.ts
+++ src/configs/config.ts
@@ -1,2 +1,2 @@
-//export const apiurl = 'https://dotnet-sql.azurewebsites.net/api/';
-export const apiurl = 'https://localhost:7193/api/';
+export const apiurl = 'https://dotnet-sql.azurewebsites.net/api/';
+//export const apiurl = 'https://localhost:7193/api/';
